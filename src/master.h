00925887b9f592e39ef860fa6757ac8fd70ba3cf
xulw <xuliangwei@wangtengtech.com>
2024-08-16 16:03:42 +0800

    docs:comment for function over class master

--- src/master.h
+++ src/master.h
@@ -28,32 +28,53 @@ class Master {
         : frame_(serial_connection, apci_parameters) { running_ = false; }
     ~Master() { ; }
 
+    /// @brief Initialize the environment of commucation
+    /// NOTE: This function has to be called at begin
     void Start();
 
+    /// @brief Release the environment of commucation
+    /// NOTE: This function has to be called at end
     void Stop();
 
+    /// @brief Start the commucation
+    /// NOTE: This function has to be called after start
     void StartDT();
 
+    /// @brief Reset the commucation
+    /// NOTE: This function has to be called after startdt to notify the peer that it is ready
     void ResetDT();
 
+    /// @brief Stop the commucation
+    /// NOTE: This function has to be called before stop
     void StopDT();
 
+    /// @brief Send data to peer by serial
+    /// @param data the msg buffer to be send
+    /// @param size the size of buffer
     void SendFrame(uint8_t* data, int size);
 
+    /// @brief Register a callback handler for received connection event
+    /// @param handler user provided callback handler function
     void SetConnectionHandler(ConnectionEventHandler handler);
 
+    /// @brief Register a callback handler for received msg
+    /// @param serial_receiver user provided callback handler function
     void SetRecviverHandler(MessageReceivedHandler serial_receiver);
 
    private:
+    /// @brief Main thread function that runs the main loop.
     void MainThread();
 
+    /// @brief Callback handler function for I-frame
+    /// @param parameter provided parameter that is passed to the callback handler
+    /// @param msg the msg received by serial
+    /// @param size the size of msg
+    /// @param more whether has more data
     bool DefaultRecviverHandler(uint8_t* msg, int size, bool more);
 
+    /// @brief Callback handler function for U-frame
+    /// @param frame frame type
     bool ConnectionHandler(UFrame frame);
-
-    void Store(uint8_t* data, int size);
-
-    void SendData();
 
    private:
     Frame frame_;
